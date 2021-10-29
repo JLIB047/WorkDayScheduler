ee64b3ee5d4e913f9a13056a78116b83636d054b
Jason Libertelli <jason.libertelli@gmail.com>
2021-10-28 23:22:00 -0400

    localstorage

--- Develop/script.js
+++ Develop/script.js
@@ -15,11 +15,29 @@ $(document).ready(function(){
         
     })
 
+    var timeTracker = function() {
+        //get the current time 
+        var nowTime = moment().hour();
+        //loop over class time block 
+        $("#time-block").each(function(){
+            var blockTime = parseInt($(this).attr("id").split("hour"))
+        })
+    }
     //check the time and add the classes for background indicators
     // as the day goes by the List will update background colors 
      
 
     // get items from local storage 
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 })
 
 
